b0f14fa3da35a57e4beb8defddbbeba11fee8515
DamsMoug06 <damien.mougenot06@gmail.com>
2023-11-22 10:27:33 +0100

    Bouton + video

--- Site trailer/javascript/Site.js
+++ Site trailer/javascript/Site.js
@@ -1,12 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var video = document.getElementById('videoPlayer');
-    var volumeControl = document.getElementById('volume');
-    var playButton = document.getElementById('playButton');
-    volumeControl.addEventListener('input', function () {
-        video.volume = volumeControl.value;
-    });
-});
-
 function ignorerVideo() {
     alert("Vidéo ignorée!");
 }
