39f2f757544afb41ac03bda58476b2ed99e6091d
DamsMoug06 <damien.mougenot06@gmail.com>
2024-02-21 12:24:48 +0100

    Magnifique

--- Site trailer/javascript/Site.js
+++ Site trailer/javascript/Site.js
@@ -1,4 +1,12 @@
-function ignorerVideo() {
+/* function ignorerVideo() {
     alert("Vidéo ignorée!");
 }
-document.getElementById("ignorerBtn").addEventListener("click", ignorerVideo);
+document.getElementById("ignorerBtn").addEventListener("click", ignorerVideo);  */
+
+const loginForm = document.getElementById('loginForm');
+
+const loginBtn = document.getElementById('loginBtn');
+loginBtn.addEventListener('click', function() {
+    document.getElementById('loginBtn').className = "hidden";
+    document.getElementById('loginForm').className = "loginForm";
+});
